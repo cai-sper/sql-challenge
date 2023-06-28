675bf2bfdca93a54dee41d70fcc457568193da67
cai-sper <cailin.sperling@gmail.com>
2023-06-28 18:58:09 -0400

    ERD Uploaded

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -37,8 +37,24 @@ ON de.emp_no=e.emp_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 
+SELECT first_name, last_name, sex 
+FROM employees
+WHERE first_name='Hercules'
+AND last_name like 'B%'
+
 -- List each employee in the Sales department, including their employee number, last name, and first name.
 
+SELECT emp_no, first_name, last_name 
+FROM employees
+WHERE emp_no IN
+	(SELECT emp_no
+	FROM dept_emp
+	WHERE dept_no IN
+		(SELECT dept_no
+		FROM departments
+		WHERE dept_name='Sales'))
+
 -- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
