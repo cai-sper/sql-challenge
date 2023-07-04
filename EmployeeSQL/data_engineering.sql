cc8bc1ab9475f478ac24a5e9ce11e5f12fc71dd4
cai-sper <cailin.sperling@gmail.com>
2023-07-03 22:43:21 -0400

    Data engineering update

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,15 +1,29 @@
+-- Drop tables if they exist
+DROP TABLE IF EXISTS 
+	salaries,
+	dept_emp,
+	dept_manager,
+	employees,
+	titles,
+	departments;
+
+-- Create departments table
 CREATE TABLE departments (
 	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR NOT NULL,
+	dept_name VARCHAR,
 	PRIMARY KEY (dept_no)
 );
 
+-- Create titles table
 CREATE TABLE titles (
 	title_id VARCHAR NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR,
 	PRIMARY KEY (title_id)
 );
 
+-- Create employees table
+-- Change date type to MDY to enable importation of data (default is DMY)
+ALTER DATABASE "challenge_DB" SET datestyle TO "ISO, MDY";
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	emp_title_id VARCHAR NOT NULL,	
@@ -22,6 +36,7 @@ CREATE TABLE employees (
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
+-- Create dept_manager table
 CREATE TABLE dept_manager (
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
@@ -29,12 +44,14 @@ CREATE TABLE dept_manager (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- Create salaries table
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
+-- Create dept_emp table
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
