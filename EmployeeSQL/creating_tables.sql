32fc86d1832db9180cf007492493301e9b4da6ac
cai-sper <cailin.sperling@gmail.com>
2023-07-03 22:11:59 -0400

    updated table

--- EmployeeSQL/creating_tables.sql
+++ EmployeeSQL/creating_tables.sql
@@ -1,25 +1,23 @@
 CREATE TABLE departments (
 	dept_no VARCHAR NOT NULL,
-	dept_name VARCHAR,
+	dept_name VARCHAR NOT NULL,
 	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE titles (
 	title_id VARCHAR NOT NULL,
-	title VARCHAR,
+	title VARCHAR NOT NULL,
 	PRIMARY KEY (title_id)
 );
 
---Error occured during import when hire_date was assigned as DATE, so changed to VARCHAR
---Will modify the datatype during the analysis process
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	emp_title_id VARCHAR NOT NULL,	
-	birth_date VARCHAR(10),
-	first_name VARCHAR,
-	last_name VARCHAR,	
+	birth_date DATE,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),	
 	sex VARCHAR(1),
-	hire_date VARCHAR,
+	hire_date DATE,
 	PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
